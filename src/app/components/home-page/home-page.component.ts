021d15c49b118b728d99c5a33b04199d2ba38722
unknown <jorge.seminario.dev@gmail.com>
2019-10-09 19:55:30 -0500

    update

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../services/data.service';
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../services/data.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -9,19 +9,48 @@ import { Observable } from 'rxjs';
 })
 export class HomePageComponent implements OnInit {
 
-  pokemons: Object;
+  allPokemons: any[] = [];
+  pokemon: any = {};
+  loading: boolean;
+
   constructor(
     private data: DataService
-    ) { }
-
-  ngOnInit() {
-    this.data.getPokemons().subscribe(
-      response => this.pokemons = response.results
-    )
+  ) {
+    this.loadPoke();
   }
 
-  onPokemonLoad() {
-    console.log("1");
-  }  
+  ngOnInit() {
+
+  }
+
+  onPokemonLoad(objPoke: any) {
+    this.pokemon = objPoke;
+    console.log(this.pokemon);
+  }
+
+  orderAz() {
+    this.allPokemons.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+  }
+
+  private loadPoke() {
+    this.loading = true;
+
+    this.data.getAll().then((res: any) => {
+      this.loading = true;
+      this.allPokemons = res;
+      if (this.allPokemons.length > 0) {
+        this.loading = false;
+      }
+    });
+  }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,76 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor( 
-    private http: HttpClient
-    ) { }
+  resultAllPokemon: any[] = [];
+  newAllPokemonList: any[] = [];
 
-    getPokemons(){
-      return this.http.get('https://pokeapi.co/api/v2/pokemon/?limit=964');      
-      //return this.http.get('https://pokeapi.co/api/v2/pokemon');      
-    }
+  obj: any = {};
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getPokemons() {
+    const url = `https://pokeapi.co/api/v2/pokemon/?limit=964`;
+    return this.http.get(url);
+  }
+
+  getOnePokemon(name: string) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return new Promise((resolve, reject) => {
+      const URL = `https://pokeapi.co/api/v2/pokemon/?limit=964`;
+      this.http.get(URL).subscribe((res: any) => {
+        this.resultAllPokemon = res.results;
+
+        // https://pokeapi.co/api/v2/pokemon/10023/
+        // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+        // ["https:", "", "pokeapi.co", "api", "v2", "pokemon", "2", ""]
+
+        for (let i = 0; this.resultAllPokemon.length > i; i++) {
+          const URL_FOTO = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+          const separado = this.resultAllPokemon[i].url.split('/');
+          const id = separado[6];
+
+          // tslint:disable-next-line:no-shadowed-variable
+          this.getPokemonInformation(id).then((res: any) => {
+            this.obj = res;
+          }).then(() => {
+            const objPokemon = {
+              id,
+              name: this.resultAllPokemon[i].name,
+              info: this.obj,
+              urlFoto: `${URL_FOTO}/${id}.png`
+            };
+            this.newAllPokemonList.push(objPokemon);
+            resolve(this.newAllPokemonList);
+          });
+        }
+      }, (err: HttpErrorResponse) => {
+        console.log(err);
+      });
+    });
+  }
+
+  getPokemonInformation(id: number) {
+    return new Promise((resolve, reject) => {
+      const URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      this.http.get(URL).subscribe((res: any) => {
+        resolve(res);
+      }, (err: HttpErrorResponse) => {
+        console.log(err);
+      });
+    });
+  }
+
+
 }
